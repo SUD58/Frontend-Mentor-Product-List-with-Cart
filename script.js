b5f4129a27be1ebc608171c206f7de8fd4ef861d
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-19 15:45:36 +0530

    Install lit
    HTML syntax wasn't being highlighted in VS Code.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+import { html, render } from "lit";
+
 const itemsList = document.querySelector("#items-list");
 const cartNumber = document.querySelector("#cart-number");
 let cartNumberOfItems = 0;
@@ -5,27 +7,30 @@ let cartNumberOfItems = 0;
 fetch("./data.json")
   .then((response) => response.json())
   .then((data) => {
-    console.log(data);
-    data.forEach((item) => {
-      itemsList.insertAdjacentHTML(
-        "beforeend",
-        `<li id="items-list" class="flex flex-col">
-          <div class="relative mb-8">
-            <img src="${item.image.mobile}" alt="" class="rounded-2xl" />
-            
-            <button
-              class="add-to-cart-button group absolute inset-x-0 -bottom-5 mx-auto flex w-fit gap-2 rounded-full border border-Frontend-Rose-500 bg-white px-8 py-2 hover:border-Frontend-Red"
-            >
-              <img src="assets/images/icon-add-to-cart.svg" alt="" />
-              <p class="font-bold group-hover:text-Frontend-Red">Add to Cart</p>
-            </button>
-          </div>
-          <h4 class="text-Frontend-Rose-300">${item.category}</h4>
-          <h3 class="text-lg font-bold">${item.name}</h3>
-          <p class="font-bold text-Frontend-Red">$${item.price}</p>
-        </li>`,
-      );
-    });
+    const itemsTemplate = html`
+      ${data.map(
+        (item) =>
+          html`<li id="items-list" class="flex flex-col">
+            <div class="relative mb-8">
+              <img src="${item.image.mobile}" alt="" class="rounded-2xl" />
+
+              <button
+                class="add-to-cart-button group absolute inset-x-0 -bottom-5 mx-auto flex w-fit gap-2 rounded-full border border-Frontend-Rose-500 bg-white px-8 py-2 hover:border-Frontend-Red"
+              >
+                <img src="assets/images/icon-add-to-cart.svg" alt="" />
+                <p class="font-bold group-hover:text-Frontend-Red">
+                  Add to Cart
+                </p>
+              </button>
+            </div>
+            <h4 class="text-Frontend-Rose-300">${item.category}</h4>
+            <h3 class="text-lg font-bold">${item.name}</h3>
+            <p class="font-bold text-Frontend-Red">$${item.price.toFixed(2)}</p>
+          </li>`,
+      )}
+    `;
+    render(itemsTemplate, itemsList);
+
     const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
     console.log(addToCartButtons);
 
@@ -53,7 +58,7 @@ fetch("./data.json")
         addToCartButtonSelected.classList.add(
           ...addToCartButtonSelectedClasses,
         );
-        addToCartButtonSelected.innerHTML = `<button
+        addToCartButtonSelected.innerHTML = html`<button
                 class="group flex aspect-square items-center justify-center rounded-full border p-2 hover:bg-white"
               >
                 <svg class="aspect-square w-4 group-hover:stroke-Frontend-Red">
