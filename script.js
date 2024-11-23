6cbfd0fa14b388331a70785c6dbb0eceb6708dff
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-23 16:45:46 +0530

    Make order total functional and full cart layout operational
    Add to cart button was adding to cart but the rest
    of the cart layout was not being shown.

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
 const itemsList = document.querySelector("#items-list");
-const cartItemsList = document.querySelector("#cart-items-list");
+const cartActive = document.querySelector("#cart-active");
+const cartInactive = document.querySelector("#cart-inactive");
+const cartItems = document.querySelector("#cart-items");
+const totalPrice = document.querySelector("#total-price");
 let cartQuantity = 0;
+let totalPriceAmount = 0;
 const cartQuantitySpan = document.querySelector("#cart-number");
 cartQuantitySpan.textContent = cartQuantity;
 const items = [];
@@ -11,8 +15,6 @@ fetch("./data.json")
     items.push(...data);
     createItemsList();
   });
-
-console.log(items);
 
 function createItemsList() {
   items.forEach((item) => {
@@ -51,6 +53,15 @@ function createAddToCartButton() {
   `;
 }
 
+function attachAddToCartListeners() {
+  const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
+  addToCartButtons.forEach((button, index) => {
+    button.addEventListener("click", function () {
+      addToCart(this, index);
+    });
+  });
+}
+
 function addToCart(button, index) {
   const buttonDiv = button.parentNode;
   button.remove();
@@ -60,52 +71,46 @@ function addToCart(button, index) {
   buttonDiv.innerHTML = createQuantityControlDiv(itemQuantity);
 
   const cartItemsTemplate = `
-    <div
-      class="flex max-h-60 flex-col gap-4 overflow-auto text-sm"
-    >
-      <div
-        class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
-      >
-        <div class="flex flex-col gap-2">
-          <h4 class="font-bold">${items[index].name}</h4>
-          <div class="flex gap-2">
-            <p class="font-bold text-Frontend-Red">${itemQuantity}</p>
-            <p class="text-Frontend-Rose-400">@$${items[index].price}</p>
-            <p class="font-bold text-Frontend-Rose-400">${itemQuantity * items[index].price}</p>
-          </div>
-        </div>
-        <button>
-          <img
-            src="assets/images/icon-remove-item.svg"
-            alt=""
-            class="rounded-full border border-Frontend-Rose-300 p-0.5"
-          />
-        </button>
-      </div>
-    </div>
+  <div
+  class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
+  >
+  <div class="flex flex-col gap-2">
+  <h4 class="font-bold">${items[index].name}</h4>
+  <div class="flex gap-2">
+  <p class="font-bold text-Frontend-Red">${itemQuantity}</p>
+  <p class="text-Frontend-Rose-400">@$${items[index].price}</p>
+  <p class="font-bold text-Frontend-Rose-400">${itemQuantity * items[index].price}</p>
+  </div>
+  </div>
+  <button class="group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
+  <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
+          <use href="#remove-item-icon" ></use>
+        </svg>
+  </button>
+  </div>
     `;
-  if (cartQuantity === 0) {
-    cartItemsList.innerHTML = cartItemsTemplate;
-    cartQuantity++;
-    cartQuantitySpan.textContent = cartQuantity;
-  } else {
-    cartItemsList.insertAdjacentHTML("beforeend", cartItemsTemplate);
-    cartQuantity++;
-    cartQuantitySpan.textContent = cartQuantity;
-  }
+  cartQuantity++;
+  cartQuantitySpan.textContent = cartQuantity;
+
+  cartActive.classList.remove("hidden");
+  cartInactive.classList.add("hidden");
+  cartItems.insertAdjacentHTML("beforeend", cartItemsTemplate);
+
+  totalPriceAmount += items[index].price;
+  totalPrice.textContent = `$${totalPriceAmount.toFixed(2)}`;
 }
 
 function createQuantityControlDiv(quantity) {
   return `
     <div class="absolute inset-x-0 -bottom-5 mx-auto flex w-fit items-center justify-between gap-14 rounded-full bg-Frontend-Red px-2 py-2 text-white">
-      <button class="group flex aspect-square items-center justify-center rounded-full border p-2 hover:bg-white" onclick="decrementItem(this.parentNode, this.parentNode.parentNode)">
-        <svg class="aspect-square w-3 group-hover:stroke-Frontend-Red">
+      <button class="group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white" onclick="decrementItem(this.parentNode, this.parentNode.parentNode)">
+        <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
           <use href="#decrement-icon"></use>
         </svg>
       </button>
       <p class="text-xl font-bold">${quantity}</p>
-      <button class="group flex aspect-square items-center justify-center rounded-full border p-2 hover:bg-white" onclick="incrementItem(this.parentNode)">
-        <svg class="aspect-square w-3 group-hover:stroke-Frontend-Red">
+      <button class="group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white" onclick="incrementItem(this.parentNode)">
+        <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
           <use href="#increment-icon"></use>
         </svg>
       </button>
@@ -136,14 +141,4 @@ function decrementItem(addedToCartDiv, buttonDiv) {
   } else {
     itemQuantityParagraph.textContent = itemQuantity;
   }
-}
-
-function attachAddToCartListeners() {
-  const addToCartButtons = document.querySelectorAll(".add-to-cart-button");
-
-  addToCartButtons.forEach((button, index) => {
-    button.addEventListener("click", function () {
-      addToCart(this, index);
-    });
-  });
 }
