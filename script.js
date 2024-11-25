745f45faf6776d8529c5a0ff5d20fded4c1f7530
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-25 13:46:05 +0530

    Make cartItems array to populate cartItemsLIst

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 const itemsList = document.querySelector("#items-list");
 const cartActive = document.querySelector("#cart-active");
 const cartInactive = document.querySelector("#cart-inactive");
-const cartItems = document.querySelector("#cart-items");
+const cartItemsList = document.querySelector("#cart-items");
 const totalPrice = document.querySelector("#total-price");
 let cartQuantity = 0;
 let totalPriceAmount = 0;
 const cartQuantitySpan = document.querySelector("#cart-number");
 cartQuantitySpan.textContent = cartQuantity;
 const items = [];
+const cartItems = [];
 
 fetch("./data.json")
   .then((response) => response.json())
@@ -17,9 +18,7 @@ fetch("./data.json")
   });
 
 function createItemsList() {
-  items.forEach((item) => {
-    console.log(item);
-
+  items.map((item) => {
     const itemsTemplate = `
     <li class="flex flex-col">
       <div class="relative mb-8">
@@ -66,37 +65,51 @@ function addToCart(button, index) {
   const buttonDiv = button.parentNode;
   button.remove();
 
-  let itemQuantity = 1;
+  cartItems.push(items[index]);
 
-  buttonDiv.innerHTML = createQuantityControlDiv(itemQuantity);
-
-  const cartItemsTemplate = `
-  <div
-  class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
-  >
-  <div class="flex flex-col gap-2">
-  <h4 class="font-bold">${items[index].name}</h4>
-  <div class="flex gap-2">
-  <p class="font-bold text-Frontend-Red">${itemQuantity}</p>
-  <p class="text-Frontend-Rose-400">@$${items[index].price}</p>
-  <p class="font-bold text-Frontend-Rose-400">${itemQuantity * items[index].price}</p>
-  </div>
-  </div>
-  <button class="group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
-  <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
-          <use href="#remove-item-icon" ></use>
-        </svg>
-  </button>
-  </div>
-    `;
   cartQuantity++;
   cartQuantitySpan.textContent = cartQuantity;
 
   cartActive.classList.remove("hidden");
   cartInactive.classList.add("hidden");
-  cartItems.insertAdjacentHTML("beforeend", cartItemsTemplate);
+  populateCartItemsList(cartItems, cartItemsList, buttonDiv);
+  setTotalPrice(cartItems);
+}
 
-  totalPriceAmount += items[index].price;
+function populateCartItemsList(cartItems = [], cartItemsList, buttonDiv) {
+  cartItemsList.innerHTML = cartItems
+    .map((cartItem) => {
+      cartItem.quantity = 1;
+      buttonDiv.innerHTML = createQuantityControlDiv(cartItem.quantity);
+      const cartItemsTemplate = `
+      <li
+      class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
+      >
+        <div class="flex flex-col gap-2">
+          <h4 class="font-bold">${cartItem.name}</h4>
+          <div class="flex gap-2">
+            <p class="font-bold text-Frontend-Red">${cartItem.quantity}</p>
+            <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
+            <p class="font-bold text-Frontend-Rose-400">${cartItem.quantity * cartItem.price}</p>
+          </div>
+        </div>
+        <button class="group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
+          <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
+            <use href="#remove-item-icon" ></use>
+          </svg>
+        </button>
+      </li>
+        `;
+      return cartItemsTemplate;
+    })
+    .join("");
+}
+
+function setTotalPrice(cartItems = []) {
+  const initialTotal = 0;
+  const totalPriceAmount = cartItems.reduce((total, cartItem) => {
+    return total + cartItem.price;
+  }, initialTotal);
   totalPrice.textContent = `$${totalPriceAmount.toFixed(2)}`;
 }
 
