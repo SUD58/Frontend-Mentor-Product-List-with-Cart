bd94132ae171ad4c184f61bd072be9204b6e1684
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-26 16:57:17 +0530

    Create order confirmation modal and make confirm order functionality

--- script.js
+++ script.js
@@ -5,6 +5,13 @@ const cartInactive = document.querySelector("#cart-inactive");
 const cartItemsList = document.querySelector("#cart-items");
 const totalPrice = document.querySelector("#total-price");
 const cartQuantitySpan = document.querySelector("#cart-number");
+const confirmOrderButton = document.querySelector("#confirm-order-button");
+const orderConfirmationModal = document.querySelector(
+  "#order-confirmation-modal",
+);
+const newOrderButton = document.querySelector("#new-order-button");
+const orderItemsList = document.querySelector("#order-items-list");
+const orderTotal = document.querySelector("#order-total");
 
 // State Variables
 let cartQuantity = 0;
@@ -38,20 +45,20 @@ function createItemElement(item, index) {
   li.className = "flex flex-col";
 
   li.innerHTML = `
-    <div class="relative mb-8">
-      <picture class="rounded-2xl">
-        <source srcset="${item.image.mobile}">
-        <source srcset="${item.image.tablet}" media="(min-width: 1024px)">
-        <source srcset="${item.image.desktop}" media="(min-width: 1280px)">
-        <img src="${item.image.thumbnail}" class="rounded-2xl">
-      </picture>
-      <div data-item-id="${index}" class="button-div">
-        ${getButtonHTML(li, index)}
-      </div>
-    </div>
-    <h4 class="text-Frontend-Rose-300">${item.category}</h4>
-    <h3 class="text-lg font-bold">${item.name}</h3>
-    <p class="font-bold text-Frontend-Red">$${item.price.toFixed(2)}</p>
+  <div class="relative mb-8">
+  <picture class="rounded-2xl">
+  <source srcset="${item.image.mobile}">
+  <source srcset="${item.image.tablet}" media="(min-width: 1024px)">
+  <source srcset="${item.image.desktop}" media="(min-width: 1280px)">
+  <img src="${item.image.thumbnail}" class="rounded-2xl">
+  </picture>
+  <div data-item-id="${index}" class="button-div">
+  ${getButtonHTML(li, index)}
+  </div>
+  </div>
+  <h4 class="text-Frontend-Rose-300">${item.category}</h4>
+  <h3 class="text-lg font-bold">${item.name}</h3>
+  <p class="font-bold text-Frontend-Red">$${item.price.toFixed(2)}</p>
   `;
 
   // Attach button listeners
@@ -80,13 +87,13 @@ function getButtonHTML(li, index) {
 // Create Add to Cart button
 function createAddToCartButton() {
   return `
-    <button
+  <button
       class="add-to-cart-button group absolute inset-x-0 -bottom-5 mx-auto flex w-fit gap-2 rounded-full border border-Frontend-Rose-500 bg-white px-8 py-2 hover:border-Frontend-Red"
     >
       <img src="assets/images/icon-add-to-cart.svg" alt="" />
       <p class="font-bold group-hover:text-Frontend-Red">Add to Cart</p>
-    </button>
-  `;
+      </button>
+      `;
 }
 
 // Handle Add to Cart logic
@@ -113,19 +120,19 @@ function renderQuantityControls(buttonDiv, itemId) {
 // Create quantity control HTML
 function createQuantityControlHTML(quantity) {
   return `
-    <div class="quantity-control-div absolute inset-x-0 -bottom-5 mx-auto flex w-fit items-center justify-between gap-14 rounded-full bg-Frontend-Red px-2 py-2 text-white">
-      <button class="decrement-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
-        <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
-          <use href="#decrement-icon"></use>
-        </svg>
-      </button>
-      <p class="text-xl font-bold">${quantity > 1 ? quantity : 1}</p>
-      <button class="increment-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
-        <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
-          <use href="#increment-icon"></use>
-        </svg>
-      </button>
-    </div>
+  <div class="quantity-control-div absolute inset-x-0 -bottom-5 mx-auto flex w-fit items-center justify-between gap-14 rounded-full bg-Frontend-Red px-2 py-2 text-white">
+  <button class="decrement-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
+  <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
+  <use href="#decrement-icon"></use>
+  </svg>
+  </button>
+  <p class="text-xl font-bold">${quantity > 1 ? quantity : 1}</p>
+  <button class="increment-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
+  <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
+  <use href="#increment-icon"></use>
+  </svg>
+  </button>
+  </div>
   `;
 }
 
@@ -195,22 +202,22 @@ function updateCartList() {
 // Create individual cart item HTML
 function createCartItemHTML(cartItem) {
   return `
-    <li data-item-id=${cartItem.id} class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4">
-      <div class="flex flex-col gap-2">
-        <h4 class="font-bold">${cartItem.name}</h4>
-        <div class="flex gap-2">
-          <p class="font-bold text-Frontend-Red">${cartItem.quantity}</p>
-          <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
-          <p class="font-bold text-Frontend-Rose-400">$${(cartItem.quantity * cartItem.price).toFixed(2)}</p>
-        </div>
-      </div>
-      <button class="remove-item-button group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
-        <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
+  <li data-item-id=${cartItem.id} class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4">
+  <div class="flex flex-col gap-2">
+  <h4 class="font-bold">${cartItem.name}</h4>
+  <div class="flex gap-2">
+  <p class="font-bold text-Frontend-Red">${cartItem.quantity}x</p>
+  <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
+  <p class="font-bold text-Frontend-Rose-400">$${(cartItem.quantity * cartItem.price).toFixed(2)}</p>
+  </div>
+  </div>
+  <button class="remove-item-button group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
+  <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
           <use href="#remove-item-icon"></use>
-        </svg>
-      </button>
-    </li>
-  `;
+          </svg>
+          </button>
+          </li>
+          `;
 }
 
 // Update total price
@@ -220,6 +227,7 @@ function updateTotalPrice() {
     0,
   );
   totalPrice.textContent = `$${totalPriceAmount.toFixed(2)}`;
+  orderTotal.textContent = `$${totalPriceAmount.toFixed(2)}`;
 }
 
 // Remove cart item using remove button
@@ -239,3 +247,37 @@ cartItemsList.addEventListener("click", (event) => {
     updateTotalPrice();
   }
 });
+
+confirmOrderButton.addEventListener("click", () => {
+  orderConfirmationModal.showModal();
+  orderItemsList.innerHTML = cartItems.map(createOrderItemHTML).join("");
+});
+
+newOrderButton.addEventListener("click", () => {
+  orderConfirmationModal.close();
+});
+
+function createOrderItemHTML(cartItem) {
+  return `
+    <li
+      data-item-id="${cartItem.id}"
+      class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
+    >
+      <div class="flex items-center gap-2">
+        <img
+          src="${cartItem.image.thumbnail}"
+          alt=""
+          class="w-16 rounded-md"
+        />
+        <div class="flex flex-col gap-2">
+          <h4 class="font-bold">${cartItem.name}</h4>
+          <div class="flex gap-2">
+            <p class="font-bold text-Frontend-Red">${cartItem.quantity}x</p>
+            <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
+          </div>
+        </div>
+      </div>
+      <p class="font-bold text-Frontend-Rose-400">$${(cartItem.quantity * cartItem.price).toFixed(2)}</p>
+    </li>
+            `;
+}
