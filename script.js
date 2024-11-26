52ac5b5fc893b11b3d633114bdce1e11bd5f6fe4
Suhrud Shringarputale <49031424+SUD58@users.noreply.github.com>
2024-11-26 10:56:47 +0530

    Make increment and decrement buttons work

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ fetch("./data.json")
   });
 
 function createItemsList() {
-  items.map((item) => {
+  items.map((item, index) => {
     const itemsTemplate = `
     <li class="flex flex-col">
       <div class="relative mb-8">
@@ -28,7 +28,7 @@ function createItemsList() {
           <source srcset="${item.image.desktop}" media="(min-width: 1280px)">
           <img src="${item.image.thumbnail}" class="rounded-2xl">
         </picture>
-        <div class="button-div">
+        <div data-item-id=${index} class="button-div">
           ${createAddToCartButton()}
         </div>
       </div>
@@ -63,9 +63,14 @@ function attachAddToCartListeners() {
 
 function addToCart(button, index) {
   const buttonDiv = button.parentNode;
+
   button.remove();
 
   cartItems.push(items[index]);
+  cartItemIndex = cartItems.indexOf(items[index]);
+  cartItems[cartItemIndex].quantity = 1; //Set item quantity
+  cartItems[cartItemIndex].id = index;
+  console.log(cartItems[cartItemIndex].id);
 
   cartQuantity++;
   cartQuantitySpan.textContent = cartQuantity;
@@ -73,56 +78,58 @@ function addToCart(button, index) {
   cartActive.classList.remove("hidden");
   cartInactive.classList.add("hidden");
   populateCartItemsList(cartItems, cartItemsList, buttonDiv);
+
   setTotalPrice(cartItems);
 }
 
 function populateCartItemsList(cartItems = [], cartItemsList, buttonDiv) {
   cartItemsList.innerHTML = cartItems
     .map((cartItem) => {
-      cartItem.quantity = 1;
       buttonDiv.innerHTML = createQuantityControlDiv(cartItem.quantity);
       const cartItemsTemplate = `
       <li
       class="flex items-center justify-between border-b border-Frontend-Rose-100 pb-4"
       >
-        <div class="flex flex-col gap-2">
-          <h4 class="font-bold">${cartItem.name}</h4>
-          <div class="flex gap-2">
-            <p class="font-bold text-Frontend-Red">${cartItem.quantity}</p>
-            <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
-            <p class="font-bold text-Frontend-Rose-400">${cartItem.quantity * cartItem.price}</p>
-          </div>
-        </div>
-        <button class="group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
-          <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
-            <use href="#remove-item-icon" ></use>
-          </svg>
-        </button>
+      <div class="flex flex-col gap-2">
+      <h4 class="font-bold">${cartItem.name}</h4>
+      <div class="flex gap-2">
+      <p class="font-bold text-Frontend-Red">${cartItem.quantity}</p>
+      <p class="text-Frontend-Rose-400">@$${cartItem.price}</p>
+      <p class="font-bold text-Frontend-Rose-400">${cartItem.quantity * cartItem.price}</p>
+      </div>
+      </div>
+      <button class="group flex aspect-square items-center justify-center rounded-full border border-Frontend-Rose-300 p-0.5 hover:border-Frontend-Rose-500">
+      <svg class="aspect-square w-3 fill-Frontend-Rose-300 group-hover:fill-Frontend-Rose-500">
+      <use href="#remove-item-icon" ></use>
+      </svg>
+      </button>
       </li>
-        `;
+      `;
       return cartItemsTemplate;
     })
     .join("");
+  increment(buttonDiv);
+  decrement(buttonDiv);
 }
 
 function setTotalPrice(cartItems = []) {
   const initialTotal = 0;
   const totalPriceAmount = cartItems.reduce((total, cartItem) => {
-    return total + cartItem.price;
+    return total + cartItem.price * cartItem.quantity;
   }, initialTotal);
   totalPrice.textContent = `$${totalPriceAmount.toFixed(2)}`;
 }
 
 function createQuantityControlDiv(quantity) {
   return `
-    <div class="absolute inset-x-0 -bottom-5 mx-auto flex w-fit items-center justify-between gap-14 rounded-full bg-Frontend-Red px-2 py-2 text-white">
-      <button class="group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white" onclick="decrementItem(this.parentNode, this.parentNode.parentNode)">
+    <div class="added-to-cart-div absolute inset-x-0 -bottom-5 mx-auto flex w-fit items-center justify-between gap-14 rounded-full bg-Frontend-Red px-2 py-2 text-white">
+      <button class="decrement-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
         <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
           <use href="#decrement-icon"></use>
         </svg>
       </button>
       <p class="text-xl font-bold">${quantity}</p>
-      <button class="group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white" onclick="incrementItem(this.parentNode)">
+      <button class="increment-button group flex aspect-square items-center justify-center rounded-full border p-1 hover:bg-white">
         <svg class="aspect-square w-3 fill-white group-hover:fill-Frontend-Red">
           <use href="#increment-icon"></use>
         </svg>
@@ -131,27 +138,51 @@ function createQuantityControlDiv(quantity) {
   `;
 }
 
-function incrementItem(addedToCartDiv) {
-  const itemQuantityParagraph = addedToCartDiv.querySelector("p");
-  let itemQuantity = parseInt(itemQuantityParagraph.textContent, 10);
-  itemQuantity++;
-  cartQuantity++;
-  itemQuantityParagraph.textContent = itemQuantity;
-  cartQuantitySpan.textContent = cartQuantity;
+function increment(buttonDiv) {
+  const incrementButton = buttonDiv.querySelector(".increment-button");
+  const itemQuantityParagraph = buttonDiv.querySelector("p");
+  incrementButton.addEventListener("click", () => {
+    const cartItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.id == buttonDiv.dataset.itemId,
+    );
+
+    cartItems[cartItemIndex].quantity++;
+    cartQuantity++;
+    cartQuantitySpan.textContent = cartQuantity;
+    itemQuantityParagraph.textContent = cartItems[cartItemIndex].quantity;
+    populateCartItemsList(cartItems, cartItemsList, buttonDiv);
+    setTotalPrice(cartItems);
+  });
 }
 
-function decrementItem(addedToCartDiv, buttonDiv) {
-  const itemQuantityParagraph = addedToCartDiv.querySelector("p");
-  let itemQuantity = parseInt(itemQuantityParagraph.textContent, 10);
-  itemQuantity--;
-  cartQuantity--;
-  cartQuantitySpan.textContent = cartQuantity;
+function decrement(buttonDiv) {
+  const decrementButton = buttonDiv.querySelector(".decrement-button");
+  const itemQuantityParagraph = buttonDiv.querySelector("p");
+  const addedToCartDiv = buttonDiv.querySelector(".added-to-cart-div");
+  decrementButton.addEventListener("click", () => {
+    const cartItemIndex = cartItems.findIndex(
+      (cartItem) => cartItem.id == buttonDiv.dataset.itemId,
+    );
+    cartItems[cartItemIndex].quantity--;
+    cartQuantity--;
+    cartQuantitySpan.textContent = cartQuantity;
 
-  if (itemQuantity < 1) {
-    addedToCartDiv.remove();
-    buttonDiv.innerHTML = createAddToCartButton();
-    attachAddToCartListeners();
-  } else {
-    itemQuantityParagraph.textContent = itemQuantity;
-  }
+    if (cartItems[cartItemIndex].quantity < 1) {
+      cartItems.splice(cartItemIndex, 1);
+      populateCartItemsList(cartItems, cartItemsList, buttonDiv);
+      setTotalPrice(cartItems);
+      if (cartItems.length === 0) {
+        cartActive.classList.add("hidden");
+        cartInactive.classList.remove("hidden");
+      }
+
+      addedToCartDiv.remove();
+      buttonDiv.innerHTML = createAddToCartButton();
+      attachAddToCartListeners();
+    } else {
+      itemQuantityParagraph.textContent = cartItems[cartItemIndex].quantity;
+      populateCartItemsList(cartItems, cartItemsList, buttonDiv);
+      setTotalPrice(cartItems);
+    }
+  });
 }
